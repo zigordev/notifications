ce9dc45ccd410dd692363e7580199384b469fa4b
zlz104107 <zigor.lopez@dehn.de>
2026-03-11 20:44:29 +0100

    feat: add cv contact email handling

--- src/main/java/com/zigordev/notifications/mail/GmailEmailSender.java
+++ src/main/java/com/zigordev/notifications/mail/GmailEmailSender.java
@@ -13,11 +13,14 @@ public class GmailEmailSender {
     this.mailSender = mailSender;
   }
 
-  public void send(String from, String to, RenderedEmail email) throws Exception {
+  public void send(String from, String to, String replyTo, RenderedEmail email) throws Exception {
     MimeMessage message = mailSender.createMimeMessage();
     MimeMessageHelper helper = new MimeMessageHelper(message, "UTF-8");
     helper.setFrom(from);
     helper.setTo(to);
+    if (replyTo != null && !replyTo.isBlank()) {
+      helper.setReplyTo(replyTo);
+    }
     helper.setSubject(email.subject());
     helper.setText(email.html(), true);
     mailSender.send(message);
